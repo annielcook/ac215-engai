fce37cb16a85887a025a870854d9d94d592f6b4a
Nevil George <liven93@gmail.com>
2023-09-24 20:23:29 -0400

    Add docker-shell.sh for preprocessing/ + implement validation in cv_val.py

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -1,1 +1,37 @@
-# preprocess.py
+from google.cloud import storage
+from io import StringIO
+import json
+
+RAW_DATA_BUCKET_NAME="team-engai"
+PROCESSED_DATA_BUCKET_NAME="team-engai-processed-data"
+FIELDS=['class_description', 'class_name', 'fields', 'methods_info']
+PYTHON_PROCESSED_DATA_FILE = 'ClassEval_data_processed.json'
+
+## initialize client and get blobs from bucket 
+client = storage.Client.from_service_account_json('secrets/data-service-account.json')
+bucket = client.get_bucket(RAW_DATA_BUCKET_NAME)
+blobs = bucket.list_blobs()
+
+finalArr = []
+
+## for each blob keep the fields mentioned in field
+for blob in blobs:
+    blob = bucket.blob(blob.name)
+    blob = blob.download_as_string()
+    blob = blob.decode('utf-8')
+    blob = StringIO(blob)  #
+    jsonArray = json.loads(blob.getvalue()) 
+    
+    counter = 0 
+    newData = {}
+    for data in jsonArray:
+        for field in FIELDS:
+            newData[field] = data[field]
+
+        finalArr.append(newData)
+
+## write processed data to processed data bucket
+processed_data_bucket = client.get_bucket(PROCESSED_DATA_BUCKET_NAME)
+blob = processed_data_bucket.blob(PYTHON_PROCESSED_DATA_FILE)
+
+blob.upload_from_string(str(finalArr))
--- src/validation/cv_val.py
+++ src/validation/cv_val.py
@@ -1,1 +1,43 @@
 from sklearn.model_selection import train_test_split
+from google.cloud import storage
+from io import StringIO
+import json
+
+PROCESSED_DATA_BUCKET_NAME="team-engai-processed-data"
+TRAINING_DATA_BUCKET_NAME="team-engai-training-data"
+TESTING_DATA_BUCKET_NAME="team-engai-testing-data"
+FIELDS=['class_description', 'class_name', 'fields', 'methods_info']
+PYTHON_PROCESSED_DATA_FILE = 'ClassEval_data_processed.json'
+
+# initialize client and get blobs from bucket 
+client = storage.Client.from_service_account_json('secrets/data-service-account.json')
+bucket = client.get_bucket(PROCESSED_DATA_BUCKET_NAME)
+blobs = bucket.list_blobs()
+
+all_data = []
+# for each blob keep the fields mentioned in field
+for blob in blobs:
+    blob = bucket.blob(blob.name)
+    blob = blob.download_as_string()
+    blob = blob.decode('utf-8')
+    blob = StringIO(blob)  #
+    json_array = json.loads(blob.getvalue()) 
+    
+    counter = 0 
+    new_data = {}
+    for data in json_array:
+        for field in FIELDS:
+            new_data[field] = data[field]
+
+        all_data.append(new_data)
+
+# Train with 90% of samples, test with 10% .
+training_set, testing_set = train_test_split(all_data, test_size=0.1)
+
+training_data_bucket = client.get_bucket(TRAINING_DATA_BUCKET_NAME)
+training_blob = training_data_bucket.blob(PYTHON_PROCESSED_DATA_FILE)
+training_blob.upload_from_string(str(training_set))
+
+testing_data_bucket = client.get_bucket(TESTING_DATA_BUCKET_NAME)
+testing_blob = testing_data_bucket.blob(PYTHON_PROCESSED_DATA_FILE)
+testing_blob.upload_from_string(str(testing_set))
