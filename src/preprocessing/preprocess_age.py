5deed88e5a44101f902cc435bd9ad35220977656
Annie Landefeld <anniecook@ostby.com>
2023-10-05 20:24:57 -0400

    Add preprocessing files from Curren

---
+++ src/preprocessing/preprocess_age.py
@@ -0,0 +1,56 @@
+from google.cloud import storage
+from google.colab import drive
+import torch
+from torchvision import transforms
+from PIL import Image
+import PIL
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import io
+import torchvision as tv
+
+def resize_img(fn, blb, proc_bkt):
+	blb.download_to_filename(fn)
+	image = Image.open(fn)
+	convert_tensor = transforms.ToTensor()
+	image_tensor = convert_tensor(image)
+	image_tensor = image_tensor.permute(1, 2, 0)
+	image_tensor = image_tensor.unsqueeze(0)
+	image_tensors = tf.image.resize(image_tensor, [224, 224], method=tf.image.ResizeMethod.BILINEAR, preserve_aspect_ratio=False)
+	img = tf.image.convert_image_dtype(image_tensors[0], dtype=tf.uint8)
+	PIL.Image.fromarray(np.array(img)).save(fn)
+	destination_blob = proc_bkt.blob(blb.name)
+	destination_blob.upload_from_filename(fn)
+
+RAW_AGE_NAME="team-engai-dogs"
+RAW_AGE_PREF="dog_age_dataset/Expert_Train/Expert_TrainEval"
+client = storage.Client.from_service_account_json('./secrets/data-service-account.json')
+blobs_age = client.list_blobs(RAW_AGE_NAME, prefix=RAW_AGE_PREF)
+
+PROC_AGE_NAME="team-engai-dogs-processed"
+proc_age = client.get_bucket(PROC_AGE_NAME)
+
+drive.mount('/content/drive')
+
+blobs_age = list(blobs_age)
+
+for blob in blobs_age:
+  try:
+    if ".DS_Store" not in blob.name:
+      if re.search(r"Adult",blob.name):
+        print("Adult")
+        label = "Adult"
+      elif re.search(r"Senior",blob.name):
+        print("Senior")
+        label = "Senior"
+      elif re.search(r"Young",blob.name):
+        print("Young")
+        label = "Young"
+      if(not blob.name.endswith("/")):
+        file_name = blob.name.split('/')[-1].split('.')[0] + "_" + label + ".jpg"
+        resize_img(file_name, blob, proc_age)
+  except:
+    print("got exception" + blob.name)
+    continue
---
+++ src/preprocessing/preprocess_breed.py
@@ -0,0 +1,44 @@
+from google.cloud import storage
+from google.colab import drive
+import torch
+from torchvision import transforms
+from PIL import Image
+import PIL
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import io
+import torchvision as tv
+
+def resize_img(fn, blb, proc_bkt):
+	blb.download_to_filename(fn)
+	image = Image.open(fn)
+	convert_tensor = transforms.ToTensor()
+	image_tensor = convert_tensor(image)
+	image_tensor = image_tensor.permute(1, 2, 0)
+	image_tensor = image_tensor.unsqueeze(0)
+	image_tensors = tf.image.resize(image_tensor, [224, 224], method=tf.image.ResizeMethod.BILINEAR, preserve_aspect_ratio=False)
+	img = tf.image.convert_image_dtype(image_tensors[0], dtype=tf.uint8)
+	PIL.Image.fromarray(np.array(img)).save(fn)
+	destination_blob = proc_bkt.blob(blb.name)
+	destination_blob.upload_from_filename(fn)
+
+RAW_BREED_NAME="team-engai-dogs"
+RAW_BREED_PREF="dog_breed_dataset/images/Images"
+client = storage.Client.from_service_account_json('./secrets/data-service-account.json')
+# bucket = client.get_bucket(RAW_DATA_BUCKET_NAME,prefix=RAW_BUCKET)
+blobs_breed = client.list_blobs(RAW_BREED_NAME, prefix=RAW_BREED_PREF)
+
+PROC_BREED_NAME="team-engai-dogs-processed"
+proc_breed = client.get_bucket(PROC_BREED_NAME)
+
+drive.mount('/content/drive')
+
+blobs_breed = list(blobs_breed)
+
+for blob in blobs_breed:
+  if ".DS_Store" not in blob.name:
+      if(not blob.name.endswith("/")):
+        file_name = blob.name.split('/')[-1]
+        resize_img(file_name, blob, proc_breed)
