44010517c450af7a0a06bf4a1be88444b5c2c76e
Annie Landefeld <anniecook@ostby.com>
2023-10-24 17:46:53 -0400

    Update to use tensorflow instead of pytorch and move helpers into util file

--- src/preprocessing/preprocess_breed.py
+++ src/preprocessing/preprocess_breed.py
@@ -1,25 +1,7 @@
-import re
-import tensorflow as tf
-import numpy as np
-
 from google.cloud import storage
-from torchvision import transforms
-from PIL import Image
 
+from util import resize_img
 
-def resize_img(blb, proc_bkt, curr_ext):
-    local_image_file = 'curr_image' + curr_ext
-    blb.download_to_filename(local_image_file)
-    image = Image.open(local_image_file)
-    convert_tensor = transforms.ToTensor()
-    image_tensor = convert_tensor(image)
-    image_tensor = image_tensor.permute(1, 2, 0)
-    image_tensor = image_tensor.unsqueeze(0)
-    image_tensors = tf.image.resize(image_tensor, [224, 224], method=tf.image.ResizeMethod.BILINEAR, preserve_aspect_ratio=False)
-    img = tf.image.convert_image_dtype(image_tensors[0], dtype=tf.uint8)
-    Image.fromarray(np.array(img)).save(local_image_file)
-    destination_blob = proc_bkt.blob(blb.name)
-    destination_blob.upload_from_filename(local_image_file)
 
 RAW_BREED_NAME="team-engai-dogs"
 RAW_BREED_PREF="dog_breed_dataset/images/Images"
@@ -40,7 +22,6 @@ for blob in blobs_breed:
           if blob.name.endswith('png'):
               curr_ext = '.png'
         file_name = blob.name.split('/')[-1]
-        # resize_img(file_name, blob, proc_breed, curr_ext)
         resize_img(blob, proc_breed, curr_ext)
 
 print('Resizing complete!')
