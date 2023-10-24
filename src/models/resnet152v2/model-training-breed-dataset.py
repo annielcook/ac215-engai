fd0fdb843402daa80abfc8d65991b0b9c351b546
Abhijit Pujare <abhijitpujare@dhcp-10-250-184-238.harvard.edu>
2023-10-24 16:44:34 -0400

    Adding code to training breed dataset and fixing model-training container to run both scripts

--- src/models/resnet152v2/model-training-breed-dataset.py
+++ src/models/resnet152v2/model-training-breed-dataset.py
@@ -7,6 +7,7 @@ from PIL import Image
 # Google
 from google.cloud import storage
 
+
 # Data
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -21,6 +22,7 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 import tensorflow as tf
 # Transfer Learning Models
 from tensorflow.keras.applications import ResNet152V2, InceptionV3
+
 
 def parse_tfrecord_example(example_proto):
   parsed_example = tf.io.parse_single_example(example_proto, feature_description)
@@ -44,19 +46,18 @@ def normalize(image, label):
 
 # Connect to GCS Bucket
 TENSORIZED_DATA_BUCKET_NAME="team-engai-dogs-tensorized"
-client = storage.Client.from_service_account_json('secrets/data-service-account.json')
-adult_blobs = client.list_blobs(TENSORIZED_DATA_BUCKET_NAME, prefix='dog_age_dataset/Expert_Train/Expert_TrainEval/Adult')
-senior_blobs = client.list_blobs(TENSORIZED_DATA_BUCKET_NAME, prefix='dog_age_dataset/Expert_Train/Expert_TrainEval/Senior')
-young_blobs = client.list_blobs(TENSORIZED_DATA_BUCKET_NAME, prefix='dog_age_dataset/Expert_Train/Expert_TrainEval/Young')
+client = storage.Client.from_service_account_json('./secrets/data-service-account.json')
+blobs = client.list_blobs(TENSORIZED_DATA_BUCKET_NAME, prefix='dog_breed_dataset/images/Images')
 
-breed_directory_name = 'age-data'
+
+breed_directory_name = 'breed-data'
 
 if not os.path.exists(breed_directory_name):
     os.mkdir(breed_directory_name)
 
 
-train_directory_name = 'age-data/train'
-validation_directory_name = 'age-data/validate'
+train_directory_name = 'breed-data/train'
+validation_directory_name = 'breed-data/validate'
 
 if not os.path.exists(train_directory_name):
     os.mkdir(train_directory_name)
@@ -64,20 +65,25 @@ if not os.path.exists(train_directory_name):
 if not os.path.exists(validation_directory_name):
     os.mkdir(validation_directory_name)
 
-CLASSES = ['Adult', 'Senior', 'Young']
+breed_to_image_files = {}
 
-for CLASS in CLASSES:
-  count = 0
-  blobs = client.list_blobs(TENSORIZED_DATA_BUCKET_NAME, prefix='dog_age_dataset/Expert_Train/Expert_TrainEval/' + CLASS)
-  total_blobs = sum(1 for _ in blobs)
-  split_point = int(total_blobs * 0.8)
-  blobs = client.list_blobs(TENSORIZED_DATA_BUCKET_NAME, prefix='dog_age_dataset/Expert_Train/Expert_TrainEval/' + CLASS)
-  for blob in blobs:
-    if count < split_point:
-      blob.download_to_filename(f'{train_directory_name}/{blob.name.split("/")[-1]}')
+for blob in blobs:
+  image_file_name = blob.name.split('/')[-1]
+  breed_name = blob.name.split('/')[-2]
+  if breed_name not in breed_to_image_files:
+    breed_to_image_files[breed_name] = []
+  breed_to_image_files[breed_name].append(image_file_name)
+
+for breed_name in breed_to_image_files:
+  num_images = len(breed_to_image_files[breed_name])
+  num_train_images = int(num_images * 0.8)
+  for i in range(num_images):
+    blob = client.get_bucket(TENSORIZED_DATA_BUCKET_NAME).blob(f'dog_breed_dataset/images/Images/{breed_name}/{breed_to_image_files[breed_name][i]}')
+    if i < num_train_images:
+      blob.download_to_filename(f'{train_directory_name}/{breed_to_image_files[breed_name][i]}')
     else:
-      blob.download_to_filename(f'{validation_directory_name}/{blob.name.split("/")[-1]}')
-    count += 1
+      blob.download_to_filename(f'{validation_directory_name}/{breed_to_image_files[breed_name][i]}')
+
 
 # Create a dictionary with the image data and label
 feature_description = {
@@ -90,10 +96,12 @@ feature_description = {
 num_channels = 3
 image_height = 224
 image_width = 224
-num_classes = 3
+num_classes = len(breed_to_image_files)
 batch_size = 32
 
-os.chdir('age-data')
+os.chdir('breed-data')
+
+
 
 # Read the tfrecord files
 train_tfrecord_files = tf.data.Dataset.list_files('train/*')
@@ -114,7 +122,7 @@ validate_data = validate_data.prefetch(buffer_size=tf.data.AUTOTUNE)
 # Pretrained Model
 base_model = ResNet152V2(include_top=False, input_shape=(224,224,3), weights='imagenet')
 base_model.trainable = False # Freeze the Weights
-BREED_COUNT = 3
+BREED_COUNT = len(breed_to_image_files)
 
 # Model Name
 name1 = "DogNetV1"
@@ -143,3 +151,8 @@ DogNetV1.fit(
     callbacks=callbacks,
     verbose=1
 )
+
+
+
+
+
--- src/models/resnet152v2/run-model.sh
+++ src/models/resnet152v2/run-model.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 pipenv run python3 model-training-age-dataset.py
+pipenv run python3 model-training-breed-dataset.py
