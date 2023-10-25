1a13dcee6625f31fd21ef34f11a88d225ed4a6fb
Annie Landefeld <anniecook@ostby.com>
2023-10-25 13:25:20 -0400

    Fix channels to be 3 always instead of sometimes being 4 randomly

--- src/preprocessing/util.py
+++ src/preprocessing/util.py
@@ -7,14 +7,12 @@ from PIL import Image
 def to_tensor(image):
     # Convert image dtype from uint8 to float32 and scale values from [0, 255] to [0, 1]
     image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-    # Transpose the image from (H, W, C) to (C, H, W)
-    # image = tf.transpose(image, (2, 0, 1))
     return image
 
 def resize_img(blb, proc_bkt, curr_ext):
     local_image_file = 'curr_image' + curr_ext
     blb.download_to_filename(local_image_file)
-    image = Image.open(local_image_file)
+    image = Image.open(local_image_file).convert('RGB')
     image_tensor = to_tensor(image)
     image_tensor = tf.expand_dims(image_tensor, axis=0)
     image_tensors = tf.image.resize(image_tensor, [224, 224], method=tf.image.ResizeMethod.BILINEAR, preserve_aspect_ratio=False)
