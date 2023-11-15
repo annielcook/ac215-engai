09b00aab8fa973f695065155b837e589af7a134d
Nevil George <liven93@gmail.com>
2023-11-14 22:05:33 -0500

    Remove unnecessary code

--- src/api-service/api/model.py
+++ src/api-service/api/model.py
@@ -7,11 +7,11 @@ class ModelController(object):
     """
     ModelController keeps track of which model is being used for
     DawgAI and is responsible for making prediction requests for
-    inference
+    inference.
 
     Features:
-    * Calls VertexAI endppoint for prediction using remote model.
-    * Downloads model from WandB and stores locally for local prediction.
+    * Calls VertexAI endpoint for prediction using remote model.
+    * (TODO) Downloads model from WandB and stores locally for local prediction.
     """
 
     def __init__(self, model_config, index_to_breed_map) -> None:
@@ -46,30 +46,3 @@ class ModelController(object):
             'max_probability_index': max_probability_index,
             'predicted_breed': predicted_breed,
         }
-
-
-if __name__ == '__main__':
-    project = "32226619505"
-    location = "us-central1"
-    endpoint_id = "6394181284029005824"
-    model_id = "4385650617012453376"
-
-    # Create an AI Platform client
-    aiplatform.init(project=project, location=location)
-
-    # Get the model and endpoint
-    model = aiplatform.Model(model_name=f"projects/{project}/locations/{location}/models/{model_id}")
-    endpoint = aiplatform.Endpoint(endpoint_name=f"projects/{project}/locations/{location}/endpoints/{endpoint_id}")
-
-    with open('dog.jpeg', 'rb') as f:
-        image_bytes = base64.b64encode(f.read()).decode('utf-8')
-
-    instances = [{'bytes_inputs': {'b64': image_bytes}}]
-
-    # Send a prediction request
-    response = endpoint.predict(instances=instances)
-    print(f'Num predictions: {len(response.predictions[0])}')
-
-    # Print the prediction response
-    print("Prediction response:")
-    print(response)
