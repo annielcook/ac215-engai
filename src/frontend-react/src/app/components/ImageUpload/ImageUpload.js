e317828ddb312e6ffe6e477ee37eb6c95ed9c7d6
Curren Iyer <curren.iyer@gmail.com>
2023-11-19 18:24:12 -0500

    adding proper folder hierarchy

--- src/frontend-react/src/app/components/ImageUpload/ImageUpload.js
+++ src/frontend-react/src/app/components/ImageUpload/ImageUpload.js
@@ -48,11 +48,14 @@ const ImageUpload = ({ onPredictionChange }) => {
           setSelectedImage(null);
           return;
       }
+      // Use the local model if set, default to remote model.
+      const useLocalModel = selectedModel === 'local-model';
       const formData = new FormData();
       formData.append('image', selectedImage);
       formData.append('file_type', fileType);
+      formData.append('use_local_model', useLocalModel);
 
-      DataService.Predict(formData, selectedModel)
+      DataService.Predict(formData)
           .then(function(response) {
             console.log(response.data.predicted_breed);
             console.log(response.data.max_probability);
